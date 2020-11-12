e2785cd110efc94e258e0efeb4f50f4918501c6f
Vitaly Lavrov <lavrovvv@gmail.com>
2020-11-12 15:10:41 +0100

    Use reflection to check presence of EvoVersion

--- prometheus-client-test-scalatest30/src/main/scala/com/evolutiongaming/prometheusclienttest/PrometheusClientSuite.scala
+++ prometheus-client-test-scalatest30/src/main/scala/com/evolutiongaming/prometheusclienttest/PrometheusClientSuite.scala
@@ -1,12 +1,11 @@
 package com.evolutiongaming.prometheusclienttest
 
-import io.prometheus.client.EvoVersion
 import org.scalatest.FunSuite
 
 
 trait PrometheusClientSuite extends FunSuite {
 
   test("EvoVersion must be on classpath as proof that the client is patched") {
-    val _ = EvoVersion.version
+    getClass.getClassLoader.loadClass("io.prometheus.client.EvoVersion")
   }
 }
--- prometheus-client-test/src/main/scala/com/evolutiongaming/prometheusclienttest/PrometheusClientSuite.scala
+++ prometheus-client-test/src/main/scala/com/evolutiongaming/prometheusclienttest/PrometheusClientSuite.scala
@@ -1,12 +1,11 @@
 package com.evolutiongaming.prometheusclienttest
 
-import io.prometheus.client.EvoVersion
 import org.scalatest.funsuite.AnyFunSuite
 
 
 trait PrometheusClientSuite extends AnyFunSuite {
 
   test("EvoVersion must be on classpath as proof that the client is patched") {
-    val _ = EvoVersion.version
+    getClass.getClassLoader.loadClass("io.prometheus.client.EvoVersion")
   }
 }
